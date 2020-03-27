e1e2aaa1d404ca0ad4378bbad9cd32ac669cf2e9
Denis Migdal <denis@migdal.ovh>
2020-03-27 16:42:46 +0100

    Add contact info

--- src/websites/migdal.ovh/preproc.js
+++ src/websites/migdal.ovh/preproc.js
@@ -1,22 +1,80 @@
 
-
 function __PRESCRIPT__($, options, context = {}) {
 
-	let menu = JSON.parse( fs.readFileSync(context.__dir__ + '/pages/meta.json', 'utf8') );
 
-	let menu_target = $('ul#nav');
-	let content_target = $('div#pages');
+	/** Add pages **/
+	{
+		let menu = JSON.parse( fs.readFileSync(context.__dir__ + '/pages/meta.json', 'utf8') );
 
-	for(let elem of menu) {
-		let lelem = elem.toLowerCase();
+		let menu_target = $('ul#nav');
+		let content_target = $('div#pages');
 
-		menu_target.append('<li><a href="/'+ lelem +'" id="menu_'+ lelem +'">'+ elem +'</a></li>');
+		for(let elem of menu) {
+			let lelem = elem.toLowerCase();
 
-		let div = $('<div class="hidden" id="content_'+ lelem +'"></div>');
-		let content = $.parseHTML( fs.readFileSync( context.__dir__ + '/pages/' + lelem + '.html', 'utf8' ) );
-		div.append( content );
-		content_target.append(div);
+			menu_target.append('<li><a href="/'+ lelem +'" id="menu_'+ lelem +'">'+ elem +'</a></li>');
+
+			let div = $('<div class="hidden" id="content_'+ lelem +'"></div>');
+			let content = $.parseHTML( fs.readFileSync( context.__dir__ + '/pages/' + lelem + '.html', 'utf8' ) );
+			div.append( content );
+			content_target.append(div);
+		}
 	}
 
-	return html;
+
+	/** Add contacts **/
+	{
+		let contacts = JSON.parse( fs.readFileSync(context.__dir__ + '/json/contacts.json', 'utf8') );
+
+		let contact_target = $('footer>.home_contact');
+
+		let icons = {
+			mail: 'awesome-envelope-square',
+			phone: 'awesome-phone-square',
+			address: 'awesome-map-marker'
+		};
+
+		for(let key in contacts) {
+			let contact = contacts[key];
+
+			let icon = icons[contact.type];
+			let smalltitle = contact['small-title'] || contact.title;
+			smalltitle = smalltitle.replace(/\n/g, '<br/>');
+
+			let url = '';
+
+			if(contact.type == 'mail')
+				url = "mailto:" + contact.title;
+			else if(contact.type == 'phone')
+				url = "tel:" + encodeURIComponent(contact.title);
+			else
+				url = contact.url || url;
+
+			let link = $('<a class="contact_link"><span></span></a>');
+
+			link.attr('title', contact.title);
+			link.append( smalltitle );
+			link.find('span').addClass('img-rounded glyphicon logo little_logo contact_logo');
+			link.find('span').addClass(icon);
+			link.attr('href', url);
+
+			contact_target.append(link);
+		}
+	}
+
+	/** external links **/
+	{
+		for(let elem of $('a')) {
+			elem = $(elem);
+
+			let href = elem.attr('href');
+			if(!href)
+				continue;
+
+			if(href[0] != '/') {
+				elem.attr('target', '_blank');
+				elem.attr('rel', 'noreferrer noopener nofollow external');
+			}
+		}
+	}
 }
