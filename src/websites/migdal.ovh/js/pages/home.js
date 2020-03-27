8e3f0e3c382e628e7e2e5744597f217caa099d8d
Denis Migdal <denis@migdal.ovh>
2020-03-27 19:00:22 +0100

    Add teachings

--- src/websites/migdal.ovh/js/pages/home.js
+++ src/websites/migdal.ovh/js/pages/home.js
@@ -2,13 +2,10 @@ const sprites = require('./../../img/logos/sprites.json');
 
 module.exports = (html, $) => {
 
-	html.find('#home_teaching_stats').text('TODO');
-	// <?= teaching_stats()['hetd']; ?>
+	html.find('#home_teaching_stats').text( total_service(teachings) );
 
 	show_partners_icons(html.find('#home_partners'), $);
-
-	html.find('#home_teachings').append('TODO');
-	// <?php show_teachings_icons($teachings); ?>
+	show_teaching_icons(html.find('#home_teachings'), $);
 
 	html.find('#home_demos').append('TODO');
 	//  <?php show_demos(); ?>
@@ -45,4 +42,63 @@ function show_partners_icons(target, $) {
 	}
 
 	target.append(div);
+}
+
+
+const teachings = require('./../../json/teachings.json');
+
+function show_teaching_icons(target, $) {
+
+	let content = $('<div></div>');
+	content.addClass('d-flex flex-row justify-content-center flex-wrap');
+
+	for(let teaching of teachings) {
+
+		let link = $('<a></a>');
+		link.attr('title', teaching.name);
+		link.attr('href', '/teaching/' + teaching.id);
+		link.addClass('img-rounded logo big_logo teaching_logo');
+
+		let num = sprites.teachings[teaching.id] || 0;
+		let img = $('<div class="img img_teachings sprite_'+ num +'"></div>');
+		link.append(img);
+
+		content.append(link);
+	}
+
+	target.append(content);
+
+}
+
+function total_service(teachings) {
+
+	let sum = 0;
+
+	for(let v of Array.from(Object.values(teachings), e => services_stats(e) ))
+		sum += v;
+	
+	return sum;
+}
+function services_stats(teaching) {
+
+	let hetd = 0;
+
+	for(let service of teaching.services) {
+		
+		let shetd = service.hetd;
+
+		if( ! Number.isFinite(shetd) ) {
+
+			shetd = shetd.split('*');
+
+			if(shetd[0] == 'N/A' || shetd[1] == 'N/A')
+				continue;
+
+			shetd = Number(shetd[0]) * Number(shetd[1]);
+		}
+
+		hetd += shetd;
+	}
+
+	return hetd;
 }
--- src/websites/migdal.ovh/js/pages/teaching.js
+++ src/websites/migdal.ovh/js/pages/teaching.js
@@ -1,5 +1,116 @@
-module.exports = () => {
+const teachings = require('./../../json/teachings.json');
+const sprites = require('./../../img/logos/sprites.json');
 
-	console.log('ok');
+module.exports = (html, $) => {
 
+	show_teaching_icons(html.find('#teaching_icons'), $);
+
+	show_teachings(html.find('.teaching_list'), $);
 };
+
+
+function show_teaching_icons(target, $) {
+
+	let content = $('<div></div>');
+	content.addClass('d-flex flex-row justify-content-center flex-wrap');
+
+	for(let teaching of teachings) {
+
+		let link = $('<a></a>');
+		link.attr('title', teaching.name);
+		link.attr('href', '/teaching/' + teaching.id);
+		link.addClass('img-rounded logo big_logo teaching_logo');
+
+		let num = sprites.teachings[teaching.id] || 0;
+		let img = $('<div class="img img_teachings sprite_'+ num +'"></div>');
+		link.append(img);
+
+		content.append(link);
+	}
+
+	target.append(content);
+
+}
+
+
+function show_teachings(target, $) {
+
+
+	for(let teaching of teachings ) {
+
+		let panel = $('<div></div>');
+		panel.addClass("panel panel-default");
+
+		let panel_header = $('<div><strong></strong> <a href="#" class="glyphicon glyphicon-arrow-up scrolltop_btn pull-right" title="Go to top"></a></div>');
+		panel_header.addClass('panel-heading');
+		panel_header.attr('id', teaching.id);
+
+		let hetd = services_stats(teaching);
+		panel_header.find('strong').text( teaching.name + ' (' + hetd + ' HETD)');
+
+		let panel_body = $('<div><ul></ul></div>');
+		panel_body.addClass('panel-body');
+
+		let list = panel_body.find('ul');
+		for(let service of teaching.services) {
+
+			let elem = $('<li></li>');
+			elem.text( service.year + ': ' + service.school + ' (' + service.level + ' - ' + service.resp + ') [' + service.courses + 'h; ' + service.TD + 'h; ' + service.TP + 'h; ' + service.projects + 'h; ' + service.hetd + 'h]');
+
+			if(service.url) {
+				let link = $('<a href="'+service.url +'">Educational booklet <span class="glyphicon glyphicon-share"></span></a>');
+				elem.append(' (');
+				elem.append(link);
+				elem.append(')');
+			}
+
+			list.append(elem);
+		}
+
+
+		panel.append(panel_header);
+		panel.append(panel_body);
+
+		target.append(panel);
+	}
+}
+
+/*
+function teaching_stats() {
+	global $teachings;
+
+	$hetd = 0;
+
+	foreach($teachings as $teaching)
+		$hetd += services_stats($teaching)['hetd'];
+
+	$result = [
+		'hetd' => $hetd
+	];
+
+	return $result;
+}*/
+
+function services_stats(teaching) {
+
+	let hetd = 0;
+
+	for(let service of teaching.services) {
+		
+		let shetd = service.hetd;
+
+		if( ! Number.isFinite(shetd) ) {
+
+			shetd = shetd.split('*');
+
+			if(shetd[0] == 'N/A' || shetd[1] == 'N/A')
+				continue;
+
+			shetd = Number(shetd[0]) * Number(shetd[1]);
+		}
+
+		hetd += shetd;
+	}
+
+	return hetd;
+}
